ff3c383da1ace620db28c057c5780549d6f48382
Egor <egor.velikiy.02@mail.ru>
2025-12-27 02:54:39 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const puppeteer = require('puppeteer');
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', '*');
+  next();
+});
+
+app.get('/login/', (_, res) => {
+  res.send('20cf5726-c4a6-4653-b049-6e72b934e3d4');
+});
+
+app.get('/test/', async (req, res) => {
+  const targetURL = req.query.URL;
+
+  // TODO: Заранее установить в систему chromium
+  const browser = await puppeteer.launch({
+    headless: 'new',
+  })
+
+  const page = await browser.newPage();
+  await page.goto(targetURL, { waitUntil: 'networkidle2' });
+
+  await page.click('#bt');
+
+  await page.waitForFunction(() => {
+    const input = document.querySelector('#inp');
+    return input.value;
+  }, { timeout: 1000 });
+
+  const result = await page.evaluate(() => {
+    return document.querySelector('#inp').value;
+  });
+
+  await browser.close();
+
+  res.send(result);
+});
+
+const PORT = 443;
+
+app.listen(PORT);
